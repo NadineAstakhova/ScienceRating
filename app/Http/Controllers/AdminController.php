1aabbaaa53744fde3379614dd3a6146c6cb6ddcd
Nadine <nadine.astakhova@gmail.com>
2018-08-22 15:39:00 +0200

    new design of dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
 {
     public function index() {
     //TODO design
-        return view('adminPanel/adminProfile',
+        return view('adminPanel/dashboard-lite',
             array('title' => 'profile','description' => '',
                 'page' => 'profile'));
     }
